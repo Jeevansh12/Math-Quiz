4156aa3359e80a0d1f2b93d10330c16186392a15
Jeevansh12 <76777313+Jeevansh12@users.noreply.github.com>
2021-05-31 11:56:26 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -18,28 +18,14 @@ function addUser(){
 
 }
 function send() {
-    get_word = document.getElementById("word").value;
-    word = get_word.toLowerCase();
-    console.log("word in lowerCase = " + word);
-
-    charAt1 = word.charAt(1);
-    console.log(charAt1);
-    length_divide_2 = Math.floor(word.length/2);
-    charAt2 = word.charAt(length_divide_2);
-   console.log(charAt2);
-
-   length_minus_1 = word.length - 1;
-   charAt3 = word.charAt(length_minus_1);
-   console.log(charAt3);
-
-   remove_charAt1 = word.replace(charAt1, "_");
-   remove_charAt2 = remove_charAt1.replace(charAt2,"_");
-   remove_charAt3 = remove_charAt2.replace(charAt3,"_");
-   console.log(remove_charAt3);
-   question_word = "<h4 id = 'word_display'> q. "+ remove_charAt3 + "</h4>";
-   input_box = "<br> answer : <input type= 'text' id='input_check_box'>";
-   check_button = "<br><br><button class='btn btn-info' onclick='check()'>check</button>";
-   row = question_word+ input_box+check_button;
-   document.getElementById("output").innerHTML=row;
-   document.getElementById("word").value = "";
+    number1 = document.getElementById("player1").value;
+    number2 = document.getElementById("player2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_number= "<h4>"+ number1 + " X "+ number2 +"</h4>";
+    input_box = "<br>Answer:<input type = 'text' id = 'input_check_box'>";
+    check_button = "<br><br><button class = 'btn-btn-info' onclick = 'check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
 }
